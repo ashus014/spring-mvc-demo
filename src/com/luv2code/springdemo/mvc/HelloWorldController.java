0264b5301d4d71df800f5662347b5cdf013490a7
Ashu Singh <ashu.ask.singh@gmail.com>
2021-11-24 23:42:11 +0530

    Controller level Request Mapping

--- src/com/luv2code/springdemo/mvc/HelloWorldController.java
+++ src/com/luv2code/springdemo/mvc/HelloWorldController.java
@@ -3,11 +3,14 @@ package com.luv2code.springdemo.mvc;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import javax.servlet.http.HttpServletRequest;
 import java.util.Locale;
 
 
 @Controller
+@RequestMapping("/hello")
 public class HelloWorldController {
 
     //need a controller method to show the initial form
@@ -40,6 +43,22 @@ public class HelloWorldController {
         model.addAttribute("message", result);
 
         return "helloworld";
+    }
+
+    @RequestMapping("/processFormVersionThree")
+    public String processFormVersionThree(@RequestParam("studentName") String theName, Model model){
+
+        //convert the data to all CAPS
+        theName = theName.toUpperCase();
+
+        //create the message
+        String result = "Hey My Friend from V3! " + theName;
+
+        //add message to the model
+        model.addAttribute("message", result);
+
+        return "helloworld";
+
     }
 
 }
