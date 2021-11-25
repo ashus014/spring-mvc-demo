deb1cf01cf8fdfd9858e1d247bdfacaae786dcae
Ashu Singh <ashu.ask.singh@gmail.com>
2021-11-25 16:02:58 +0530

    Spring MVC Form Tags : Using LinkedList to populate the drop down

--- src/com/luv2code/springdemo/mvc/Student.java
+++ src/com/luv2code/springdemo/mvc/Student.java
@@ -1,10 +1,17 @@
 package com.luv2code.springdemo.mvc;
 
+import java.util.LinkedHashMap;
+
 public class Student {
 
     private String firstName;
     private String lastName;
     private String country;
+    private LinkedHashMap<String, String> countryOptions;
+
+    public LinkedHashMap<String, String> getCountryOptions() {
+        return countryOptions;
+    }
 
     public String getCountry() {
         return country;
@@ -16,6 +23,13 @@ public class Student {
 
     public Student(){
 
+        //populate country options : used ISO country code
+        countryOptions = new LinkedHashMap<>();
+        countryOptions.put("BR", "Brazil");
+        countryOptions.put("FR", "France");
+        countryOptions.put("DE", "Germany");
+        countryOptions.put("IN", "India");
+        countryOptions.put("UK", "United Kingdom");
     }
 
     public String getFirstName() {
